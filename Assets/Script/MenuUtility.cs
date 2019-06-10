0626340dfd8f7b3bd1fff26ec104fa9a0a20570f
Eazey <eazey@foxmail.com>
2019-06-10 17:30:56 +0800

    Add MenuUtility
    菜单工具类
    封装下API中的排序
    也可以根据项目具体的需求来添加合适的排序算法

---
+++ Assets/Script/MenuUtility.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using EazeyFramework.UI;
+using UnityEngine;
+
+public static class MenuUtility
+{
+    private static int _ascendSortWeight;
+    private const int ASCEND = 1;
+    private const int DESCEND = -1;
+
+    public static void Sort<T>(ref List<T> list, bool ascend = true)
+        where T : MenuData
+    {
+        if (list != null && list.Count > 1)
+        {
+            _ascendSortWeight = ascend ? ASCEND : DESCEND;
+            list.Sort(Comparison);
+        }
+    }
+
+    private static int Comparison<T>(T itemA, T itemB)
+        where T : MenuData
+    {
+        int result = 0;
+
+        if (itemA.SortOrder > itemB.SortOrder)
+            result = _ascendSortWeight;
+        else if (itemA.SortOrder < itemB.SortOrder)
+            result = _ascendSortWeight * -1;
+
+        return result;
+    }
+}
